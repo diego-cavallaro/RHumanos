b1da0f733f97e97d5215acb26407574d7fb030b7
diego-cavallaro <diego.cavallaro@outlook.com>
2026-08-26 15:55:45 -0300

    se ajusta el hasta fecha + hora < a now

--- app/Http/Requests/Extras/StoreExtra.php
+++ app/Http/Requests/Extras/StoreExtra.php
@@ -19,7 +19,7 @@ class StoreExtra extends FormRequest
           'Empleado' => ['required', 
                           'max:10',
                         ],
-          'Fecha' => ['required', 'date', 'before:today' ],
+          'Fecha' => ['required', 'date', 'before:now' ],
           'Motivo' => ['required'],
           'Desde' => ['required', 'date_format:H:i'],
           'Hasta' => ['required', 'date_format:H:i', 'after:Desde'],
