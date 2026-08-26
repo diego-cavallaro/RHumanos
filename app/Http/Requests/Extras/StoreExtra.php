3e0fd83683c831d3592279be43b72f7fa165b1ef
diego-cavallaro <diego.cavallaro@outlook.com>
2026-08-26 16:04:54 -0300

    se deshace el cambio

--- app/Http/Requests/Extras/StoreExtra.php
+++ app/Http/Requests/Extras/StoreExtra.php
@@ -19,7 +19,7 @@ class StoreExtra extends FormRequest
           'Empleado' => ['required', 
                           'max:10',
                         ],
-          'Fecha' => ['required', 'date', 'before:now' ],
+          'Fecha' => ['required', 'date', 'before:today' ],
           'Motivo' => ['required'],
           'Desde' => ['required', 'date_format:H:i'],
           'Hasta' => ['required', 'date_format:H:i', 'after:Desde'],
